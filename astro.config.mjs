d0b579e79561bb04d08e1b18cdc42d905fd2b017
Adrián Mujica González <mujicagadrian@gmail.com>
2026-02-17 17:47:39 +0000

    remove subdomain wedding

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bodapazyadrian.es",
+  base: "/wedding",
   vite: {
     plugins: [tailwindcss()],
   },
