94f4be5dd95c71d1a4aefcfcf0faff08e22d3017
Adrián Mujica González <mujicagadrian@gmail.com>
2026-02-17 17:44:15 +0000

    remove base prop from astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bodapazyadrian.es",
-  base: "/wedding",
   vite: {
     plugins: [tailwindcss()],
   },
